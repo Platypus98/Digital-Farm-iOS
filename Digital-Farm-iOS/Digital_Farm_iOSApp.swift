81c30e26490faa1edeedad1cb8cd859fb807a686
Ilya Golyshkov <igolyshkov@alfabank.ru>
2022-01-12 19:41:27 +0300

    Initial Commit

---
+++ Digital-Farm-iOS/Digital_Farm_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_Farm_iOSApp.swift
+//  Digital-Farm-iOS
+//
+//  Created by Илья Голышков on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Digital_Farm_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Digital-Farm-iOSTests/Digital_Farm_iOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  Digital_Farm_iOSTests.swift
+//  Digital-Farm-iOSTests
+//
+//  Created by Илья Голышков on 12.01.2022.
+//
+
+import XCTest
+@testable import Digital_Farm_iOS
+
+class Digital_Farm_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
